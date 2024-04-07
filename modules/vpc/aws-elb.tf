ca9dd922cb6d63528b96febc54ec09098d7d5e7f
AbdulSami <msami.bese22seecs@student.nust.edu.pk>
2024-04-08 00:53:37 +0500

    Adding Elastic Load Balancer

--- modules/vpc/aws-elb.tf
+++ modules/vpc/aws-elb.tf
@@ -1,32 +1,38 @@
-resource "aws_lb" "example" {
-  name               = "example-elb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.allow_tls.id]
+resource "aws_launch_configuration" "example" {
+  name          = "example-launch-config"
+  image_id      = "ami-12345678"  # Replace with your AMI ID
+  instance_type = "t2.micro"       # Replace with your instance type
+  key_name      = "your-key-pair"  # Replace with your key pair
+  security_groups = [aws_security_group.allow_tls.id]
 
-  tags = {
-    Name = "example-elb"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-resource "aws_lb_target_group" "example" {
-  name     = "example-target-group"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+resource "aws_autoscaling_group" "example" {
+  name                 = "example-autoscaling-group"
+  launch_configuration = aws_launch_configuration.example.name
+  min_size             = 1
+  max_size             = 5
+  desired_capacity     = 2
+  vpc_zone_identifier  = [aws_subnet.main.id]  # Replace with your subnet ID(s)
 
-  health_check {
-    path = "/"
+  target_group_arns = [
+    aws_lb_target_group.example.arn
+  ]
+
+  tag {
+    key                 = "Name"
+    value               = "example-autoscaling-group"
+    propagate_at_launch = true
   }
 }
 
-resource "aws_lb_listener" "example" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.example.arn
-  }
+resource "aws_autoscaling_policy" "example" {
+  name                   = "example-autoscaling-policy"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.example.name
 }
